939684755b8d0d2cb292360e6c020c87b84bc550
Mikhail Gurtsev <gurtsevmr@yandex.ru>
2020-06-08 11:54:43 +0300

    Произвел расчеты дохода и его оценку

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-let money = 60000,
+let money,
     income = 'дипломы на заказ',
-    addExpenses = 'интернет, кварплата, продукты, учеба, телефон',
-    deposit = true,
+    addExpenses,
+    deposit,
     mission = 100000,
-    period = 9,
-    budgetDay = money / 30;
+    period = 6,
+    budgetMonth,
+    achieve;
+
+money = prompt('Ваш месячный доход?');
+
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+
+deposit = confirm('Есть ли у вас депозит в банке?');
+
+expenses1 = prompt('Введите обязательную статью расходов?');
+
+amount1 = +prompt('Во сколько это обойдется?');
+
+expenses2 = prompt('Введите обязательную статью расходов?');
+
+amount2 = +prompt('Во сколько это обойдется?');
+
+budgetMonth = +money - (amount1 + amount2);
+
+budgetDay = Math.floor(budgetMonth / 30);
+
+achieve = Math.ceil(mission / budgetMonth);
 
 
     console.log(typeof(money));
@@ -15,4 +36,14 @@ let money = 60000,
     console.log('Цель заработать ' + mission + ' рублей');
     console.log(addExpenses.toLocaleLowerCase());
     console.log(addExpenses.split(', '));
-    console.log(budgetDay);
+    console.log('Бюджет на день: ' + budgetDay + ' рублей');
+    console.log('Бюджет на месяц: ' + budgetMonth + ' рублей');
+    console.log('Цель будет достигнута за: ' + achieve +' месяцев' );
+    
+
+let result = (budgetDay < 0) ? 'У вас высокий уровень дохода' :
+    (budgetDay > 1200) ? 'У вас высокий уровень дохода' :
+    (budgetDay > 600 && budgetDay <= 1200) ? 'У вас средний уровень дохода' :
+    'К сожалению у вас уровень дохода ниже среднего';
+
+console.log(result);
